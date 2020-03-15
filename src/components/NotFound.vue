004679a27c362b7b60e7bf6ed0710e7bd2b84c62
busroutemap <kotokotaro@gmail.com>
2020-03-15 21:23:29 +0900

    土台作り

---
+++ src/components/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{text}}</p>
+    <p>{{introduction}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name:'NotFound',
+  data(){
+    return{
+      text:"適切なモードを選択してください",
+      introduction:"かけこみアラートとは？という説明をします"
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
