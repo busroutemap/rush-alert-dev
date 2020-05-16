49dc9262e7b45e65e9da735e4ad8bb60aeca5bff
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-05-16 17:25:48 +0900

    コンポーネント整理に伴う修正とデザイン変更

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="app">
-    <headNav/>
-    <router-view/>
+  <div
+  id="app"
+  >
+    <headNav
+    class="hn"
+    />
+    <router-view
+    class="main"
+    />
   </div>
 </template>
 <script>
@@ -15,12 +21,22 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$border:1280px;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  // color: #2c3e50;
+  // width:100vw;
+  // height:100vh;
 }
-
+.hn{
+  @media (min-width: $border){
+    // 基準値よりも画面サイズが大きい
+    height:5vh;
+    @media(max-height:600px){
+      height:30px;
+    }
+  }
+}
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,30 +1,29 @@
 <template>
-    <div id="app" class="container">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <router-link
-            to="/"
-            class="navbar-brand">かけこみアラート</router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- コピペ感著しいので、必要に応じて後でカスタマイズすること -->
-            <div class="collapse navbar-collapse" id="navbarsExample04">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <router-link
-                        to="/"
-                        class="nav-link">HOME</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/map" class="nav-link">マップモード</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/detail" class="nav-link">詳細モード</router-link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <router-link
+        to="/"
+        class="navbar-brand">かけこみアラート</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- コピペ感著しいので、必要に応じて後でカスタマイズすること -->
+        <div class="collapse navbar-collapse" id="navbarsExample04">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <router-link to="/" class="nav-link">HOME</router-link>
+                </li>
+                <!-- <li class="nav-item">
+                    <router-link to="/#map" class="nav-link">MAP</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/#detail" class="nav-link">DETAIL</router-link>
+                </li> -->
+                <li class="nav-item">
+                    <router-link to="/help" class="nav-link">HELP</router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -36,13 +35,6 @@ export default {
 <style scoped>
 .a{
   text-decoration: none;
-}
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 
 </style>
