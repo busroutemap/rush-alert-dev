53cd961905840baa48f69750d380a75310829326
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-12 23:32:06 +0900

    LineList雛形

--- src/components/PinComponent/LineList.vue
+++ src/components/PinComponent/LineList.vue
@@ -1,19 +1,51 @@
-<template>
-    
+<template functional>
+    <div>
+        <div
+        v-for="(route,index)in routesInfo"
+        :key="route['@id']"
+        :i="index"
+        >
+            <div
+            class="routeSymbol"
+            >
+                系統愛称(未実装)
+            </div>
+            <ul>
+                <li>{{route['dc:title']}}</li>
+                <li
+                v-for="operatorSameAs in route['odpt:operator']"
+                :key="operatorSameAs"
+                >{{operator(operatorSameAs)}}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name:"LineList",
     props:{
-
+        routesInfo:{
+            type:Array
+        },
+        operators:{
+            type:Array
+        }
     },
-    components:{
-
+    computed:{
+        operator(sameAs){
+            const aimData = operators.find(obj=>{
+                returnobj["owl:sameAs"]===sameAs
+            })
+            return aimData["dc:title"];
+        }
     },
 }
 </script>
 
 <style lang="scss" scoped>
-
+.routeSymbol{
+    border:solid 2px black;
+    background-color: cadetblue;
+}
 </style>
