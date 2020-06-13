7b9a8934ff0dabd38921bb5ba12667d179536bc5
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-13 16:25:57 +0900

    UserCard暫定

--- src/components/PinComponent/UserCard.vue
+++ src/components/PinComponent/UserCard.vue
@@ -1,15 +1,41 @@
+<script>
+/**
+ * UserCard.vue
+ * 表示情報
+ * 1. 目的地までの残りm
+ * 2. 
+ * 
+ */
+</script>
 <template>
-    
+    <div>
+        <h2>利用者情報</h2>
+        <p>目的地まで残り</p>
+        <p>{{predictUserMinues}}分</p>
+        <p>(距離 : {{distances}} m)</p>
+    </div>
 </template>
 
 <script>
 export default {
     name:"UserCard",
     props:{
-
+        predictTripTime:{
+            type:Date
+        },
+        userSpeed:{
+            // 毎分m
+            type:Number
+        },
+        distances:{
+            // m
+            type:Number
+        },
     },
-    components:{
-
+    computed:{
+        predictUserMinues(){
+            return Math.round(distances/userSpeed);
+        }
     },
 }
 </script>
