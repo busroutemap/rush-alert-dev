43944d781bf8494852dd751ed8ea9a62778ae953
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-06-13 22:09:30 +0900

    petitmap雛形

--- src/components/PinComponent/PetitMap.vue
+++ src/components/PinComponent/PetitMap.vue
@@ -1,19 +1,45 @@
 <template>
-    
+    <div>
+        <div id="map"></div>
+    </div>
 </template>
 
 <script>
+import L from 'leaflet';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
+import iconUrl from 'leaflet/dist/images/marker-icon.png'
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+import 'leaflet/dist/leaflet.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: iconRetinaUrl,
+    iconUrl: iconUrl,
+    shadowUrl: shadowUrl
+});
+
+
 export default {
     name:"PetitMap",
     props:{
-
+        
     },
     components:{
 
     },
+    mounted(){
+        let map = L.map('map').setView([43.067, 141.35], 16);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 18
+        }).addTo(map);
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+#map {
+    width: 500px;
+    height: 500px;
+}
 </style>
